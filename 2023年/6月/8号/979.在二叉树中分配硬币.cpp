b1ce6eb1b9bc4a2760a380d6d0ecf9a6913f1990
dodamce <3030949391@qq.com>
2023-06-08 13:27:32 +0800

    二叉树边贡献问题

---
+++ 2023年/6月/8号/979.在二叉树中分配硬币.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=979 lang=cpp
+ *
+ * [979] 在二叉树中分配硬币
+ */
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+    int ret = 0;
+    int dfs(TreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+        int left_remain = dfs(root->left);
+        int right_remain = dfs(root->right);
+        ret += abs(left_remain) + abs(right_remain);
+        return left_remain + right_remain + root->val - 1;
+    }
+
+public:
+    int distributeCoins(TreeNode *root)
+    {
+        dfs(root);
+        return ret;
+    }
+};
+// @lc code=end
