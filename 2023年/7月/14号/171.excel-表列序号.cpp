22504dc979d81dca783e9fd7daf1458cd4abcb81
dodamce <3030949391@qq.com>
2023-07-14 23:25:59 +0800

    字符串模拟进位，26进制

---
+++ 2023年/7月/14号/171.excel-表列序号.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=171 lang=cpp
+ *
+ * [171] Excel 表列序号
+ */
+#include <string>
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+    int titleToNumber(string columnTitle)
+    {
+        long number = 0;
+        long multiple = 1;
+        for (int i = columnTitle.length() - 1; i >= 0; i--)
+        {
+            int k = columnTitle[i] - 'A' + 1;
+            number += k * multiple;
+            multiple *= 26;
+        }
+        return number;
+    }
+};
+// @lc code=end
