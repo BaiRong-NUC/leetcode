0f849a8a3c5c8c944e111846b3d7bd1539dc387d
dodamce <3030949391@qq.com>
2024-12-31 11:05:42 +0800

    将数组导入到哈希表中方便快速查找需要的值

---
+++ 2024年/12月/31号/1.两数之和.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+#include <D:\CppCode\leetcode\global\global.h>
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (hash.find(target - nums[i]) != hash.end())
+            {
+                return {hash[target - nums[i]], i};
+            }
+            else
+            {
+                hash[nums[i]] = i;
+            }
+        }
+        return {};
+    }
+};
+// @lc code=end
