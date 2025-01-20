a3835064f34781f7fc6e3e78cccdf7c846fd9fb4
dodamce <3030949391@qq.com>
2025-01-20 19:08:51 +0800

    整理常见dp思路

---
+++ 2025年/1月/20号/1143.最长公共子序列.cs
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=csharp
+ *
+ * [1143] 最长公共子序列
+ */
+
+// @lc code=start
+public class Solution
+{
+    public int LongestCommonSubsequence(string text1, string text2)
+    {
+        // dp[i][j] 表示text1[0..i]和text2[0..j]的最长公共子序列
+        int[,] dp = new int[text1.Length + 1, text2.Length + 1];
+        for (int i = 1; i <= text1.Length; i++)
+        {
+            for (int j = 1; j <= text2.Length; j++)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                {
+                    dp[i, j] = dp[i - 1, j - 1] + 1;
+                }
+                else
+                {
+                    dp[i, j] = Math.Max(dp[i, j - 1], dp[i - 1, j]);
+                }
+            }
+        }
+        return dp[text1.Length, text2.Length];
+    }
+}
+// @lc code=end
+
