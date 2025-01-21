4c67d80feb373b77b1ec7d91b5441b778d7479cd
dodamce <3030949391@qq.com>
2025-01-21 20:06:30 +0800

    补充List二分查找方法，并且自定义比较函数

--- C#STL/list.cs
+++ C#STL/list.cs
@@ -40,3 +40,27 @@ int size = numbers.Count;//获取列表大小
 Console.WriteLine("Contains 3: " + containsThree + ", index: " + indexOfThree + ", size: " + size);
 
 // numbers.Clear(); // 清空列表中的所有元素
+
+Console.WriteLine("-----------------");
+
+//List自带的二分搜索,返回索引
+List<int> sortedNumbers = new List<int> { 1, 3, 5, 7, 9 };
+
+Console.WriteLine(sortedNumbers.BinarySearch(5));
+
+//传入自定义比较方法
+List<string> names = new List<string> { "Alice", "Bob", "Charlie", "David" };
+
+// 创建一个自定义比较器，用于按字符串长度进行比较
+IComparer<string> lengthComparer = Comparer<string>.Create((x, y) => x.Length.CompareTo(y.Length));
+//搜索长度为4的字符串
+//如果未找到，它返回一个负数，该负数是大于item的第一个元素的索引的按位求补。可以使用按位求补运算符~来获取应插入元素的位置。
+int index = names.BinarySearch("1234", lengthComparer);
+if (index >= 0)
+{
+    Console.WriteLine($"找到长度为4的字符串在索引{index}");
+}
+else
+{
+    Console.WriteLine($"未找到长度为4的字符串,插入位置为{~index}");
+}
