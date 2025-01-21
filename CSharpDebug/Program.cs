144b400f30f78d6af72e74775e240f380058148c
dodamce <3030949391@qq.com>
2025-01-21 17:26:50 +0800

    调试01背包第三种变式（必须装满）

--- CSharpDebug/Program.cs
+++ CSharpDebug/Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         Console.WriteLine("Debug Start");
         Solution solution = new Solution();
-        Console.WriteLine(solution.LengthOfLongestSubsequence(new List<int> { 1, 2, 3, 4, 5 }, 9));
+        Console.WriteLine(solution.LengthOfLongestSubsequence(new List<int> { 1, 1, 5, 4, 5 }, 3));
         Console.WriteLine("Debug End");
     }
 }
